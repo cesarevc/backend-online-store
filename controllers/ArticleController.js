ad46cd2ae9ec10ca9ea68f96430df23b83587ddb
Vazquez Cesar <cesar.evc@outlook.com>
2020-04-13 15:35:50 -0500

    sale model embedded

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -30,6 +30,24 @@ export default {
             next(e);
         }
     },
+    queryCode: async (req, res, next) => {
+        try {
+            const reg = await models.Article.findOne({code: req.query.code})
+            .populate('categoria', {nombre:1});
+            if (!reg) {
+                res.status(404).send({
+                    message: 'El registro no existe'
+                });
+            } else {
+                res.status(200).json(reg);
+            }
+        } catch (error) {
+            res.status(500).send({
+                message: 'Ocurrio un error'
+            });
+            next(e);
+        }
+    },
     list: async (req, res, next) => {
         try {
             let value = req.query.value;
--- routes/article.js
+++ routes/article.js
@@ -6,6 +6,7 @@ const router = routerx();
 
 router.post('/add', auth.verifyManager,  articleController.add);
 router.get('/query', auth.verifyManager, articleController.query);
+router.get('/queryCode', auth.verifyUser, articleController.queryCode);
 router.get('/list', auth.verifyManager, articleController.list);
 router.put('/update', auth.verifyManager, articleController.update);
 router.delete('/remove', auth.verifyManager, articleController.remove);
